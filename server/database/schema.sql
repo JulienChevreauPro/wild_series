4969f77266d13e163aaaea6f927d1db68117e094
JulienChevreauPro <julienchevreau33@gmail.com>
2024-05-25 12:34:13 +0200

    harmonia quest number 5

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,3 +1,5 @@
+drop table if exists user, item, program, category;
+
 create table user (
   id int unsigned primary key auto_increment not null,
   email varchar(255) not null unique,
@@ -9,4 +11,18 @@ create table item (
   title varchar(255) not null,
   user_id int unsigned not null,
   foreign key(user_id) references user(id)
+);
+
+create table program (
+  id int unsigned primary key  auto_increment not null,
+  title varchar(100) not null,
+  synopsis text not null,
+  poster varchar(100),
+  country varchar(100),
+  year int
+);
+
+create table category (
+  id int unsigned primary key  auto_increment not null,
+  name varchar(100) not null
 );
