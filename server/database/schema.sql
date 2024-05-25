5da86b65a49446804a8a1df5d6d51707551e9aaf
JulienChevreauPro <julienchevreau33@gmail.com>
2024-05-25 13:42:28 +0200

    new fix for harmonia quest number 5

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -13,8 +13,13 @@ create table item (
   foreign key(user_id) references user(id)
 );
 
+create table category (
+  id int unsigned primary key auto_increment not null,
+  name varchar(100) not null
+);
+
 create table program (
-  id int unsigned primary key  auto_increment not null,
+  id int unsigned primary key auto_increment not null,
   title varchar(100) not null,
   synopsis text not null,
   poster varchar(100),
@@ -22,9 +27,4 @@ create table program (
   year int not null,
   category_id int unsigned not null,
   foreign key(category_id) references category(id)
-);
-
-create table category (
-  id int unsigned primary key  auto_increment not null,
-  name varchar(100) not null
 );
