8cb1d5ddda2baefe8be2d522d9438f9284713a13
JulienChevreauPro <julienchevreau33@gmail.com>
2024-05-26 19:19:45 +0200

    harmonia quest 6

--- server/app/controllers/categoryActions.js
+++ server/app/controllers/categoryActions.js
@@ -1,36 +1,44 @@
+const tables = require("../../database/tables");
+
 // Some data to make the trick
 
-const categories = [
-    {
-      id: 1,
-      name: "Science-Fiction",
-    },
-    {
-      id: 2,
-      name: "Comédie",
-    },
-  ];
+// const categories = [
+//     {
+//       id: 1,
+//       name: "Comédie",
+//     },
+//     {
+//       id: 2,
+//       name: "Science-Fiction",
+//     },
+//   ];
   
   // Declare the actions
 
-  const browse = (req, res) => {
-    if (req.query.q != null) {
-      const filteredCategories = categories.filter((category) =>
-        category.name.includes(req.query.q)
-      );
+  const browse = async (req, res) => {
+    const categoriesFromDB = await tables.category.readAll();
+
+    res.json(categoriesFromDB);
+    };
+
+  // const browse = (req, res) => {
+  //   if (req.query.q != null) {
+  //     const filteredCategories = categories.filter((category) =>
+  //       category.name.includes(req.query.q)
+  //     );
   
-      res.json(filteredCategories);
-    } else {
-      res.json(categories);
-    }
-  };
+  //     res.json(filteredCategories);
+  //   } else {
+  //     res.json(categories);
+  //   }
+  // };
 
   // *****************************************************
 
 const read = (req, res) => {
   const parsedId = parseInt(req.params.id, 10);
 
-  const category = categories.find((p) => p.id === parsedId);
+  const category = tables.category.find((p) => p.id === parsedId);
 
   if (category != null) {
     res.json(category);
